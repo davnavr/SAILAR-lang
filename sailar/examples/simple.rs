c3c321f858dfb1d0a363aadddfc37fb5822c1dc8
David N <davidcalieln@gmail.com>
2022-04-17 15:25:00 -0700

    Add input and result types to block

--- sailar/examples/simple.rs
+++ sailar/examples/simple.rs
@@ -9,9 +9,9 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut module = sailar::Module::new(Identifier::from_str("Simple")?, vec![0, 1].into_boxed_slice());
 
     let mut block_cache = block::BuilderCache::new();
-    let mut main_block_builder = block_cache.builder();
+    let mut main_block_builder = block_cache.builder(vec![type_system::FixedInt::S32.into()].into_boxed_slice(), Box::default());
     main_block_builder.emit_nop();
-    let main_block = main_block_builder.emit_ret(Box::default())?;
+    let main_block = Arc::new(main_block_builder.emit_ret(Box::default())?);
 
     let main_function = module.add_function(
         Identifier::from_str("main")?,
@@ -19,10 +19,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             vec![type_system::FixedInt::S32.into()].into_boxed_slice(),
             Box::default(),
         )),
-        function::Kind::Defined(function::Definition::new(
-            function::Body::Defined(Arc::new(main_block)),
-            Export::Yes,
-        )),
+        function::Kind::Defined(function::Definition::new(function::Body::Defined(main_block), Export::Yes)),
     )?;
 
     println!("{:?}", module);
--- sailar/src/block.rs
+++ sailar/src/block.rs
@@ -1,6 +1,7 @@
 //! Manipulation of SAILAR code blocks.
 
 use crate::instruction_set::Instruction;
+use crate::type_system;
 
 #[derive(Clone, Debug, thiserror::Error)]
 #[non_exhaustive]
@@ -14,6 +15,8 @@ pub type ValidationResult<T> = Result<T, ValidationError>;
 /// Allows the building of SAILAR code blocks.
 #[derive(Debug)]
 pub struct Builder<'b> {
+    result_types: Box<[type_system::Any]>,
+    input_types: Box<[type_system::Any]>,
     instructions: &'b mut Vec<Instruction>,
 }
 
@@ -26,19 +29,22 @@ impl<'b> Builder<'b> {
         self.instructions.push(Instruction::Break);
     }
 
-    fn validate(self) -> ValidationResult<Block> {
+    fn finish(self) -> ValidationResult<Block> {
         if self.instructions.is_empty() {
             return Err(ValidationError::EmptyBlock);
         }
 
         Ok(Block {
+            result_types: self.result_types,
+            input_types: self.input_types,
             instructions: self.instructions.clone().into_boxed_slice(),
         })
     }
 
     pub fn emit_ret<V: Into<Box<[()]>>>(self, values: V) -> ValidationResult<Block> {
         self.instructions.push(Instruction::Ret(values.into()));
-        self.validate()
+        // TODO: Check that result types match return values.
+        self.finish()
     }
 }
 
@@ -57,8 +63,10 @@ impl BuilderCache {
         }
     }
 
-    pub fn builder(&mut self) -> Builder<'_> {
+    pub fn builder(&mut self, result_types: Box<[type_system::Any]>, input_types: Box<[type_system::Any]>) -> Builder<'_> {
         Builder {
+            result_types,
+            input_types,
             instructions: &mut self.buffer,
         }
     }
@@ -72,8 +80,9 @@ impl std::default::Default for BuilderCache {
 }
 
 #[derive(Clone, Debug, Eq, Hash, PartialEq)]
-#[repr(transparent)]
 pub struct Block {
+    result_types: Box<[type_system::Any]>,
+    input_types: Box<[type_system::Any]>,
     instructions: Box<[Instruction]>,
 }
 
@@ -81,5 +90,15 @@ impl Block {
     #[inline]
     pub fn instructions(&self) -> &[Instruction] {
         &self.instructions
+    }
+
+    #[inline]
+    pub fn result_types(&self) -> &[type_system::Any] {
+        &self.result_types
+    }
+
+    #[inline]
+    pub fn input_types(&self) -> &[type_system::Any] {
+        &self.input_types
     }
 }
