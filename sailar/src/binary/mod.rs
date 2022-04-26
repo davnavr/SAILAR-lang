a8a7c214784079077122f2556d44eebcd3c325cf
David N <davidcalieln@gmail.com>
2022-04-25 17:19:57 -0700

    Add enum for record type byte

--- sailar/src/binary/mod.rs
+++ sailar/src/binary/mod.rs
@@ -1,10 +1,40 @@
 //! Contains types to abstract over the binary representation of the SAILAR format.
 
+mod num;
+
 pub mod buffer;
 pub mod signature;
 
+pub use num::{VarIntSize, InvalidVarIntSize};
+
 /// The magic number that is the start of all SAILAR module files.
 pub const MAGIC: &[u8; 6] = b"SAILAR";
+
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[repr(u8)]
+pub enum RecordType {
+    Array = 1,
+    Identifier = 2,
+    TypeSignature = 3,
+    FunctionSignature = 4,
+    Data = 5,
+    Code = 6,
+    ModuleImport = 7,
+    FunctionImport = 8,
+    StructureImport = 9,
+    GlobalImport = 10,
+    FunctionDefinition = 11,
+    StructureDefinition = 12,
+    GlobalDefinition = 13,
+    FunctionInstantiation = 14,
+    StructureInstantiation = 15,
+    Namespace = 16,
+    //ExceptionClassImport = 17,
+    //ExceptionClassDefinition = 18,
+    //AnnotationClassImport = 19,
+    //AnnotationClassDefinition = 20,
+    //DebuggingInformation = 21,
+}
 
 /// Represents an array of bytes that make up a SAILAR module.
 #[derive(Clone)]
@@ -45,87 +75,5 @@ impl std::ops::Deref for RawModule {
 impl std::fmt::Debug for RawModule {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         write!(f, "RawModule({:?})", buffer::ByteDebug::from(&self.contents))
-    }
-}
-
-#[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
-#[repr(u8)]
-pub enum LengthSize {
-    One = 0u8,
-    Two = 1,
-    Four = 2,
-}
-
-impl LengthSize {
-    pub fn maximum_length_value(self) -> usize {
-        match self {
-            Self::One => u8::MAX.into(),
-            Self::Two => u16::MAX.into(),
-            Self::Four => u32::MAX.try_into().unwrap_or(usize::MAX),
-        }
-    }
-
-    pub const fn byte_count(self) -> u8 {
-        match self {
-            Self::One => 1,
-            Self::Two => 2,
-            Self::Four => 4,
-        }
-    }
-
-    pub fn pick_largest(&mut self, other: LengthSize) {
-        if other > *self {
-            *self = other;
-        }
-    }
-
-    /// Updates the length size value if the specified length cannot fit it.
-    ///
-    /// # Panics
-    ///
-    /// As indices are currently limited to four bytes, this panics if the `length` is greater than [`u32::MAX`].
-    pub fn resize_to_fit(&mut self, length: usize) {
-        match self {
-            Self::One if length > u8::MAX.into() => *self = Self::Two,
-            Self::One => (),
-            Self::Two if length > u16::MAX.into() => *self = Self::Four,
-            Self::Two => (),
-            Self::Four if length <= u32::MAX.try_into().unwrap_or(usize::MAX) => (),
-            Self::Four => panic!("length value too large: {}", length),
-        }
-    }
-
-    pub fn resize_to_fit_many<T, I: std::iter::IntoIterator<Item = T>, F: FnMut(T) -> usize>(
-        &mut self,
-        items: I,
-        mut length_source: F,
-    ) {
-        for item in items.into_iter() {
-            self.resize_to_fit(length_source(item))
-        }
-    }
-}
-
-impl From<LengthSize> for u8 {
-    #[inline]
-    fn from(value: LengthSize) -> u8 {
-        value as u8
-    }
-}
-
-#[derive(Clone, Debug, thiserror::Error)]
-#[error("{0:#02X} is not a valid length size value")]
-pub struct InvalidLengthSize(u8);
-
-impl TryFrom<u8> for LengthSize {
-    type Error = InvalidLengthSize;
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        match value {
-            0 => Ok(Self::One),
-            1 => Ok(Self::Two),
-            2 => Ok(Self::Four),
-            _ => Err(InvalidLengthSize(value)),
-        }
     }
 }
