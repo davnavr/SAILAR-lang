fb30c092d1a7171002a05115a5aeefe9655c47e4
David N <davidcalieln@gmail.com>
2022-05-17 19:41:32 -0700

    Hook sailas_wasm error to CodeMirror lint

--- sailas_wasm/index.js
+++ sailas_wasm/index.js
@@ -1,8 +1,9 @@
 'use strict';
 const CodeMirror = require('codemirror');
-const CodeMirrorSimpleMode = require('codemirror/addon/mode/simple.js');
-//const codeMirrorLinter = require('codemirror/addon/lint/lint.js');
 const rustWebAssembly = require('./pkg');
+
+require('codemirror/addon/mode/simple.js');
+require('codemirror/addon/lint/lint.js');
 
 const assemblerModeName = "sailar";
 
@@ -14,48 +15,71 @@ CodeMirror.defineSimpleMode(assemblerModeName, {
         { regex: /(0x[0-9a-fA-F][0-9a-fA-F_]*)|(0b[01][01_]*)|([0-9][0-9_]*)/, token: 'number' },
     ],
     meta: {
-        lineComment: ';'
+        lineComment: ';',
     }
 });
 
-document.addEventListener('DOMContentLoaded', async (event) => {
+let globalAssemblyLinter;
+
+CodeMirror.registerHelper("lint", assemblerModeName, function(text) {
+    // TODO: Figure out if call to asm.assemble can go here.
+    if (globalAssemblyLinter !== undefined) {
+        return globalAssemblyLinter(text);
+    } else {
+        return [];
+    }
+});
+
+document.addEventListener('DOMContentLoaded', async (_) => {
     const output = document.getElementById('output').appendChild(document.createElement("pre"));
     output.style = "width: 100%; height: 100%; margin: 0";
 
     const editor = CodeMirror((e) => { document.getElementById('input').appendChild(e); }, {
         gutters: ["CodeMirror-lint-markers"],
         lineNumbers: true,
-        lint: true,
+        lint:  true,
         mode: assemblerModeName,
-        value: '.format major 0\n.format minor 18\n\n; Add additional directives below\n',
+        value: '.format major 0\n.format minor 18\n\n; Add additional directives below\n\n',
     });
 
     const refreshCodeEditor = setTimeout(() => editor.refresh(), 0);
 
     const initializeAssembler = rustWebAssembly.then((asm) => {
-        function appendOutputMessage(message) {
-            output.innerHTML += message;
-        }
-
-        function appendOutputError(error, locations) {
-            output.innerHTML += 'error';
-            if (locations !== null) {
-                output.innerHTML += `(${locations[0]},${locations[1]})-(${locations[2]},${locations[3]})`;
-                //editor.markText({ line: locations[0] - 1, ch: locations[1] - 1, }, { line: locations[2] - 1, ch: locations[3] - 1 }, errorMarkOptions);
-            }
-            output.innerHTML += ': ' + error + '\n';
-        }
-
         function update() {
             output.innerHTML = '';
 
-            // TODO: Clear old marks with getAllMarks()?
+            let errors = [];
+
+            function appendOutputMessage(message) {
+                if (errors.length === 0) {
+                }
+            }
+
+            function appendOutputError(error, locations) {
+                output.innerHTML += 'error';
+
+                if (locations !== null) {
+                    output.innerHTML += `(${locations[0]},${locations[1]})-(${locations[2]},${locations[3]})`;
+
+                    // Assembler location numbers start at 1, while CodeMirror expects numbers starting at 0.
+                    errors.push({
+                        from: CodeMirror.Pos(locations[0] - 1, locations[1] - 1),
+                        to: CodeMirror.Pos(locations[2] - 1, locations[3] - 1),
+                        message: error,
+                        severity: "error",
+                    });
+                }
+
+                output.innerHTML += ': ' + error + '\n';
+            }
 
             asm.assemble(editor.getValue(), appendOutputMessage, appendOutputError);
+            
+            return errors;
         }
 
         asm.register_panic_hook();
-        editor.on('change', update);
+        globalAssemblyLinter = update;
         update();
     }).catch(console.error);
 
