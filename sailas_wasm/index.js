6bb24eb679edb749f9c9509db0cc2dc22f9e6b42
David N <davidcalieln@gmail.com>
2022-05-17 18:36:57 -0700

    Add basic syntax highlighting

--- sailas_wasm/index.js
+++ sailas_wasm/index.js
@@ -1,25 +1,38 @@
 'use strict';
 const CodeMirror = require('codemirror');
+const CodeMirrorSimpleMode = require('codemirror/addon/mode/simple.js');
+//const codeMirrorLinter = require('codemirror/addon/lint/lint.js');
 const rustWebAssembly = require('./pkg');
+
+const assemblerModeName = "sailar";
+
+CodeMirror.defineSimpleMode(assemblerModeName, {
+    start: [
+        { regex: /(?:\.format|identifier)\b/, token: 'keyword' },
+        { regex: /[a-zA-Z$][a-zA-Z$]+/, token: 'atom' },
+        { regex: /;.*/, token: 'comment' },
+        { regex: /(0x[0-9a-fA-F][0-9a-fA-F_]*)|(0b[01][01_]*)|([0-9][0-9_]*)/, token: 'number' },
+    ],
+    meta: {
+        lineComment: ';'
+    }
+});
 
 document.addEventListener('DOMContentLoaded', async (event) => {
     const output = document.getElementById('output').appendChild(document.createElement("pre"));
     output.style = "width: 100%; height: 100%; margin: 0";
 
     const editor = CodeMirror((e) => { document.getElementById('input').appendChild(e); }, {
-        value: '.format major 0\n.format minor 18\n\n; Add additional directives below\n',
+        gutters: ["CodeMirror-lint-markers"],
         lineNumbers: true,
+        lint: true,
+        mode: assemblerModeName,
+        value: '.format major 0\n.format minor 18\n\n; Add additional directives below\n',
     });
 
     const refreshCodeEditor = setTimeout(() => editor.refresh(), 0);
 
     const initializeAssembler = rustWebAssembly.then((asm) => {
-        asm.register_panic_hook();
-
-        const errorMarkOptions = {
-            className: 'cm-error',
-        };
-
         function appendOutputMessage(message) {
             output.innerHTML += message;
         }
@@ -28,7 +41,7 @@ document.addEventListener('DOMContentLoaded', async (event) => {
             output.innerHTML += 'error';
             if (locations !== null) {
                 output.innerHTML += `(${locations[0]},${locations[1]})-(${locations[2]},${locations[3]})`;
-                editor.markText({ line: locations[0] - 1, ch: locations[1] - 1, }, { line: locations[2] - 1, ch: locations[3] - 1 }, errorMarkOptions);
+                //editor.markText({ line: locations[0] - 1, ch: locations[1] - 1, }, { line: locations[2] - 1, ch: locations[3] - 1 }, errorMarkOptions);
             }
             output.innerHTML += ': ' + error + '\n';
         }
@@ -41,8 +54,8 @@ document.addEventListener('DOMContentLoaded', async (event) => {
             asm.assemble(editor.getValue(), appendOutputMessage, appendOutputError);
         }
 
+        asm.register_panic_hook();
         editor.on('change', update);
-
         update();
     }).catch(console.error);
 
