c66240c02e3b2b84d5050eb4a1cfa11f3e7fca6d
David N <davidcalieln@gmail.com>
2022-05-23 13:14:34 -0700

    Add macro for easy addition of new record types

--- sailar/src/binary/record.rs
+++ sailar/src/binary/record.rs
@@ -5,33 +5,54 @@ use crate::helper::borrow::CowBox;
 use crate::{Id, Identifier};
 use std::borrow::Cow;
 
-/// Indicates what kind of content is contained in a record.
-#[derive(Clone, Copy, Debug, Eq, PartialEq)]
-#[repr(u8)]
-pub enum Type {
-    MetadataField = 0,
-    Array = 1,
-    Identifier = 2,
-    TypeSignature = 3,
-    FunctionSignature = 4,
-    Data = 5,
-    CodeBlock = 6,
-    //ModuleImport = 7,
-    //FunctionImport = 8,
-    //StructureImport = 9,
-    //GlobalImport = 10,
-    //FunctionDefinition = 11,
-    //StructureDefinition = 12,
-    //GlobalDefinition = 13,
-    //FunctionInstantiation = 14,
-    //StructureInstantiation = 15,
-    //Namespace = 16,
-    //ExceptionClassImport = 17,
-    //ExceptionClassDefinition = 18,
-    //AnnotationClassImport = 19,
-    //AnnotationClassDefinition = 20,
-    //DebuggingInformation = 21,
+macro_rules! record_types {
+    ({
+        $($(#[$case_meta:meta])* $case_name:ident$(($($case_argument_name:ident: $case_argument:ty,)*))? = $case_number:literal,)*
+    }) => {
+        /// Indicates what kind of content is contained in a record.
+        #[derive(Clone, Copy, Debug, Eq, PartialEq)]
+        #[repr(u8)]
+        pub enum Type {
+            Array = 1,
+            $($case_name = $case_number,)*
+        }
+
+        impl TryFrom<u8> for Type {
+            type Error = InvalidTypeError;
+        
+            fn try_from(value: u8) -> Result<Self, Self::Error> {
+                match value {
+                    1 => Ok(Self::Array),
+                    $(_ if value == $case_number => Ok(Self::$case_name),)*
+                    _ => Err(InvalidTypeError { value })
+                }
+            }
+        }
+
+        #[derive(Clone, Debug)]
+        #[non_exhaustive]
+        pub enum Record<'a> {
+            $($(#[$case_meta])* $case_name$(($($case_argument,)*))?,)*
+        }
+
+        impl Record<'_> {
+            pub fn record_type(&self) -> Type {
+                match self {
+                    $(Self::$case_name$(($($case_argument_name,)*))? => Type::$case_name,)*
+                }
+            }
+        }
+    };
 }
+
+record_types!({
+    MetadataField(_field: MetadataField<'a>,) = 0,
+    Identifier(_identifier: Cow<'a, Id>,) = 2,
+    TypeSignature(_signature: Cow<'a, signature::Type>,) = 3,
+    FunctionSignature(_signature: Cow<'a, signature::Function>,) = 4,
+    Data(_bytes: Cow<'a, DataArray>,) = 5,
+    CodeBlock(_code: CowBox<'a, CodeBlock<'a>>,) = 6,
+});
 
 impl From<Type> for u8 {
     fn from(value: Type) -> u8 {
@@ -43,18 +64,6 @@ impl From<Type> for u8 {
 #[error("{value:#02X} is not a valid record type")]
 pub struct InvalidTypeError {
     value: u8,
-}
-
-impl TryFrom<u8> for Type {
-    type Error = InvalidTypeError;
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        if value <= Type::CodeBlock.into() {
-            Ok(unsafe { std::mem::transmute::<u8, Self>(value) })
-        } else {
-            Err(InvalidTypeError { value })
-        }
-    }
 }
 
 #[derive(Clone, Debug)]
@@ -166,30 +175,6 @@ impl<'a> CodeBlock<'a> {
 
     pub fn instructions(&self) -> &[crate::instruction::Instruction] {
         &self.instructions
-    }
-}
-
-#[derive(Clone, Debug)]
-#[non_exhaustive]
-pub enum Record<'a> {
-    MetadataField(MetadataField<'a>),
-    Identifier(Cow<'a, Id>),
-    TypeSignature(Cow<'a, signature::Type>),
-    FunctionSignature(Cow<'a, signature::Function>),
-    Data(Cow<'a, DataArray>),
-    CodeBlock(CowBox<'a, CodeBlock<'a>>),
-}
-
-impl Record<'_> {
-    pub fn record_type(&self) -> Type {
-        match self {
-            Self::MetadataField(_) => Type::MetadataField,
-            Self::Identifier(_) => Type::Identifier,
-            Self::TypeSignature(_) => Type::TypeSignature,
-            Self::FunctionSignature(_) => Type::FunctionSignature,
-            Self::Data(_) => Type::Data,
-            Self::CodeBlock(_) => Type::CodeBlock,
-        }
     }
 }
 
