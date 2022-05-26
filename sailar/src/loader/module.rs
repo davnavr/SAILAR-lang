99d16e9a3830566db0145d40463f9104379b0fa6
David N <davidcalieln@gmail.com>
2022-05-26 14:41:29 -0700

    Remove lifetime from loader::State

--- sailar/src/loader/module.rs
+++ sailar/src/loader/module.rs
@@ -6,16 +6,20 @@ use crate::identifier::Identifier;
 
 pub type Record = record::Record<'static>;
 
+//pub struct ModuleIdentifierReference
+
 #[derive(Debug)]
 pub struct Module {
+    loader: std::sync::Weak<crate::loader::State>,
     identifiers: Vec<Identifier>,
 }
 
 impl Module {
-    pub(crate) fn from_source<S: crate::loader::Source>(source: S) -> Result<Box<Self>, S::Error> {
-        let mut module = Box::new(Self {
+    pub(crate) fn from_source<S: crate::loader::Source>(source: S) -> Result<Self, S::Error> {
+        let mut module = Self {
+            loader: Default::default(),
             identifiers: Vec::default(),
-        });
+        };
 
         source.iter_records(|record| todo!("record {:?}", record))?;
 
--- sailar/src/loader/state.rs
+++ sailar/src/loader/state.rs
@@ -3,9 +3,9 @@
 use crate::identifier;
 use crate::loader::Module;
 use std::fmt::{Debug, Formatter};
-use std::sync::Mutex;
+use std::sync::{Arc, Mutex};
 
-type ModuleLookup<'s> = elsa::FrozenMap<&'s identifier::Id, &'s Module, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>;
+//type ModuleLookup<'s> = elsa::FrozenMap<ModuleIdentifierReference, Arc<Module>, std::hash::BuildHasherDefault<rustc_hash::FxHasher>>;
 
 #[derive(Clone, Debug, thiserror::Error)]
 #[non_exhaustive]
@@ -14,39 +14,36 @@ pub enum ModuleLoadError<E> {
     SourceError(E),
 }
 
-pub type ModuleLoadResult<'state, E> = Result<&'state Module, ModuleLoadError<E>>;
+pub type ModuleLoadResult<E> = Result<Arc<Module>, ModuleLoadError<E>>;
 
-pub struct State<'state> {
-    modules: Mutex<elsa::FrozenVec<Box<Module>>>,
-    module_lookup: Mutex<ModuleLookup<'state>>,
+pub struct State {
+    modules: Mutex<Vec<Arc<Module>>>,
+    //module_lookup: Mutex<ModuleLookup>,
 }
 
-impl<'state> State<'state> {
-    pub fn force_load_module<S: crate::loader::Source>(&self, source: S) -> ModuleLoadResult<'state, S::Error> {
+impl State {
+    pub fn new() -> Arc<Self> {
+        Arc::new(Self {
+            modules: Default::default(),
+            //module_lookup: Default::default(),
+        })
+    }
+
+    pub fn force_load_module<S: crate::loader::Source>(&self, source: S) -> ModuleLoadResult<S::Error> {
         let module = Module::from_source(source).map_err(ModuleLoadError::SourceError)?;
 
         // TODO: Check module lookup to see if module with same name is already loaded
 
-        let mut module_list = self.modules.lock().unwrap();
-        Ok(module_list.push_get(module))
+        let allocated_module = Arc::new(module);
+        self.modules.lock().unwrap().push(allocated_module.clone());
+        Ok(allocated_module)
     }
 }
 
-struct ModuleDebug<'s>(&'s Mutex<elsa::FrozenVec<Box<Module>>>);
-
-impl Debug for ModuleDebug<'_> {
-    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
-        match self.0.try_lock() {
-            Ok(fields) => f.debug_list().entries(fields.iter()).finish(),
-            Err(error) => Debug::fmt(&error, f),
-        }
-    }
-}
-
-impl Debug for State<'_> {
+impl Debug for State {
     fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
         f.debug_struct("State")
-            .field("modules", &ModuleDebug(&self.modules))
+            .field("modules", &self.modules)
             .finish()
     }
 }
