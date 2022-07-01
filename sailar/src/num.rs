e5e2a49e53b7be710a83fdc8fe492d3a1c02bc52
David N <davidcalieln@gmail.com>
2022-06-30 19:51:57 -0700

    Add unsigned variable integer type

--- sailar/src/num.rs
+++ sailar/src/num.rs
@@ -1,4 +1,124 @@
 //! Numeric types in the SAILAR binary format.
+//! 
+//! # Variable-Length Encoding
+//! 
+//! The encoding of variable-width integers in the SAILAR binary format is similar to the encoding used in UTF-8 codepoints. In
+//! SAILAR, the high bits indicate the number of bytes needed to contain the integer value.
+//! 
+//! | Actual Value                          |Integer Length (bytes)|Integer Size (bits)|
+//! |---------------------------------------|----------------------|-------------------|
+//! | `0XXXXXXX`                            | `1`                  | `7`               |
+//! | `10XXXXXX XXXXXXXX`                   | `2`                  | `14`              |
+//! | `110XXXXX XXXXXXXX XXXXXXXX`          | `3`                  | `21`              |
+//! | `1110XXXX XXXXXXXX XXXXXXXX XXXXXXXX` | `4`                  | `32`              |
+//! 
+//! For simplicity, the SAILAR binary format currently only allows a maximum length of `4` for all integers.
+
+// TODO: Read and write functions for UTF-8 style variable width integers
+
+use std::num::NonZeroU32;
+
+/// An unsigned integer represented in a SAILAR binary as a 1, 2, 3, or 4 byte long integer.
+/// 
+/// For more details, see the documentation for the [`sailar::num`] module.
+#[derive(Clone, Copy, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[repr(transparent)]
+pub struct VarU28(NonZeroU32); // Actual value stored in bits 1-28, allows null pointer optimization
+
+impl VarU28 {
+    /// Creates a new unsigned integer without checking that the value can fit in 28 bits.
+    /// 
+    /// # Safety
+    /// 
+    /// Callers should ensure that the value is small enough to fit in 28 bits.
+    pub const unsafe fn new_unchecked(value: u32) -> Self {
+        Self(NonZeroU32::new_unchecked(1u32 | (value << 1)))
+    }
+
+    pub const MIN: Self = unsafe {
+        // Safety: 0 is a valid value
+        Self::new_unchecked(0)
+    };
+
+    /// The largest value that is allowed to be encoded.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// assert_eq!(VarU28::MAX.get() >> VarU28::BITS, 0);
+    /// ```
+    pub const MAX: Self = unsafe {
+        // Safety: Maximum value is always valid
+        Self::new_unchecked(0x0FFF_FFFF)
+    };
+
+    pub const BITS: u32 = 28u32;
+
+    pub const fn get(self) -> u32 {
+        self.0.get() >> 1
+    }
+
+    /// Creates a new unsigned integer, returning `None` if the value is too large to be represented in 28 bits.
+    pub const fn new(value: u32) -> Option<Self> {
+        if value & Self::MAX.get() == 0 {
+            None
+        } else {
+            unsafe {
+                // Safety: Validation above ensures value is valid
+                Some(Self::new_unchecked(value))
+            }
+        }
+    }
+
+    pub const fn from_u8(value: u8) -> Self {
+        unsafe {
+            // Safety: Single byte can always be encoded
+            Self::new_unchecked(value as u32)
+        }
+    }
+
+    /// The maximum value that can be encoded in 1 byte.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// assert_eq!(VarU28::MIN < VarU28::MAX_1);
+    /// ```
+    pub const MAX_1: Self = Self::from_u8(0x7F);
+
+    pub const fn from_u16(value: u16) -> Self {
+        unsafe {
+            // Safety: 28-bit integer can contain 16-bit integer
+            Self::new_unchecked(value as u32)
+        }
+    }
+
+    /// The maximum value that can be encoded in 2 bytes.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// assert_eq!(VarU28::MAX_2 < VarU28::MAX_3);
+    /// ```
+    pub const MAX_2: Self = Self::from_u16(0x3FF);
+
+    /// The maximum value that can be encoded in 3 bytes.
+    /// 
+    /// # Examples
+    /// 
+    /// ```
+    /// assert_eq!(VarU28::MAX_3 < VarU28::MAX_4);
+    /// ```
+    pub const MAX_3: Self = unsafe {
+        // Safety: 28-bit integer can contain 24-bit integer
+        Self::new_unchecked(0x1FFFFF)
+    };
+
+    /// The maximum value that can be encoded in 4 bytes.
+    pub const MAX_4: Self = Self::MAX;
+}
+
+
 
 #[derive(Copy, Clone, Debug, Eq, Ord, PartialEq, PartialOrd)]
 #[repr(u8)]
