35651fdae6859c94d5b52063530cd4f9172739e2
David N <davidcalieln@gmail.com>
2022-07-06 16:22:27 -0700

    Fix index variables in true_program.rs

--- sailar_load/examples/true_program.rs
+++ sailar_load/examples/true_program.rs
@@ -1,37 +1,45 @@
 use sailar::builder;
 use sailar::identifier::Id;
+use sailar::index;
 use sailar::num::VarU28;
 use sailar::record;
 use sailar::signature;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // A program that returns an exit code of 0 (success).
-    let program = {
+    let program: Vec<record::Record<'static>> = {
         let mut builder = builder::Builder::new();
 
+        static program_version: &'static [VarU28] = &[VarU28::from_u8(1), VarU28::from_u8(0)];
+
         builder.add_record(record::MetadataField::ModuleIdentifier(
-            record::ModuleIdentifier::new_borrowed(Id::try_from_str("true")?, &[VarU28::from_u8(1), VarU28::from_u8(0)]),
+            record::ModuleIdentifier::new_borrowed(Id::try_from_str("true")?, program_version),
         ));
 
         let integer_type = {
             builder.add_record(signature::Type::S32);
-            VarU28::from_u8(0).into()
+            index::TypeSignature::from(0)
         };
 
         let main_signature = {
-            builder.add_record(signature::Function::new(&[], &[integer_type]));
-            VarU28::from_u8(0).into()
+            builder.add_record(signature::Function::new([].as_slice(), [integer_type].as_slice()));
+            index::FunctionSignature::from(0)
+        };
+
+        let main_code = {
+            //builder.add_record(record::CodeBlock::from_types(register_types: CowBox<'a, [index::TypeSignature]>, input_count: usize, result_count: usize, instructions: CowBox<'a, [instruction::Instruction]>))
+            index::CodeBlock::from(0)
         };
 
         let main_definition = {
             builder.add_record(record::FunctionDefinition::new(
-                record::Export::new_export_borrowed(Id::try_from_str("main")),
+                record::Export::new_export_borrowed(Id::try_from_str("main")?),
                 main_signature,
-                body: FunctionBody<'a>,
+                record::FunctionBody::Definition(main_code),
             ));
         };
 
-        builder.into_records();
+        builder.into_records()
     };
 
     println!("{:?}", program);
