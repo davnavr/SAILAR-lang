3af1e392e771c3d9db3380d751d81c4d4c15e696
David N <davidcalieln@gmail.com>
2022-07-14 13:23:49 -0700

    Make Error a readonly struct

--- SAILARSharp/src/Interop/Error.cs
+++ SAILARSharp/src/Interop/Error.cs
@@ -1,24 +1,15 @@
 namespace SAILARSharp.Interop;
 
-using System;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading;
 
 /// <summary>
 /// <p>Encapsulates an error message produced by SAILAR.</p>
 /// <p>This class is thread safe.</p>
 /// </summary>
-public unsafe sealed class Error : IDisposable {
+public unsafe readonly struct Error {
     internal readonly struct Opaque { }
 
     internal readonly struct OpaqueMessage { }
-
-    private readonly object sync = new();
-
-    private OpaqueMessage* message;
-
-    private string? contents = null;
 
     [DllImport("SAILARCore", CallingConvention = CallingConvention.Cdecl, EntryPoint = "sailar_dispose_error", ExactSpelling = true)]
     private static extern void Dispose(Opaque* error);
@@ -32,75 +23,23 @@ public unsafe sealed class Error : IDisposable {
     [DllImport("SAILARCore", CallingConvention = CallingConvention.Cdecl, EntryPoint = "sailar_error_message_contents", ExactSpelling = true)]
     private static extern byte* GetMessageContents(OpaqueMessage* message, out nuint length);
 
-    internal Error(Opaque* error) {
-        message = GetMessage(error);
+    public string Message { get; }
+
+    private Error(string message) {
+        Message = message;
+    }
+
+    internal Error FromOpaque(Opaque* error) {
+        var message = GetMessage(error);
         Dispose(error);
+
+        nuint length;
+        byte* buffer = GetMessageContents(message, out length);
+        string contents = System.Text.Encoding.UTF8.GetString(buffer, (int)length);
+        DisposeMessage(message);
+
+        return new(contents);
     }
 
-    /// <summary>Attempts to retrieve a <see cref="Span{T}"/> over the UTF-8 contents of the error message.</summary>
-    /// <param name="contents">
-    /// When this method returns, contains a <see cref="Span{T}"/> over the message contents, or <see cref="Span{T}.Empty"/> if
-    /// the message was already disposed.
-    /// </param>
-    /// <returns>
-    /// <see langword="true"/> if the message contents were successfully retrieved; otherwise, <see langword="false"/>.
-    /// </returns>
-    public bool GetBytes(out Span<byte> contents) {
-        lock (sync) {
-            if (message == null) {
-                contents = Span<byte>.Empty;
-                return false;
-            } else {
-                nuint length;
-                byte* message = GetMessageContents(this.message, out length);
-                contents = new Span<byte>(message, (int)length);
-                return true;
-            }
-        }
-    }
-
-    /// <summary>Gets a <see cref="Span{T}"/> over the UTF-8 contents of the error message.</summary>
-    /// <exception cref="ObjectDisposedException">Thrown if the message was already disposed.</exception>
-    public Span<byte> GetBytes() {
-        Span<byte> contents;
-        if (!GetBytes(out contents)) {
-            throw new ObjectDisposedException(GetType().FullName);
-        }
-        
-        return contents;
-    }
-
-    /// <summary>Gets the error message as a <see cref="String"/>.</summary>
-    public override string ToString() {
-        lock (sync) {
-            if (message == null) {
-                return "The message was already disposed.";
-            }
-
-            contents ??= Encoding.UTF8.GetString(GetBytes());
-
-            return contents;
-        }
-    }
-
-    private void Dispose(bool disposing) {
-        try {
-            if (disposing) {
-                Monitor.Enter(sync);
-            }
-
-            if (message != null) {
-                DisposeMessage(message);
-                message = null;
-            }
-        } finally {
-            if (disposing) {
-                Monitor.Exit(sync);
-            }
-        }
-    }
-
-    public void Dispose() => Dispose(true);
-
-    ~Error() => Dispose(false);
+    public override string ToString() => Message;
 }
