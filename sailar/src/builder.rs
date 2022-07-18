9966cceed47365c1bad8a5daa8e79c53cf0f935c
David N <davidcalieln@gmail.com>
2022-07-17 17:29:59 -0700

    Update writer to use new function records

--- sailar/src/builder.rs
+++ sailar/src/builder.rs
@@ -1,10 +1,8 @@
 //! Low-level API for building SAILAR binary modules.
 
 use crate::binary::RawModule;
-use crate::instruction::{self, Instruction};
 use crate::reader;
-use crate::record::{self, Record};
-use crate::signature;
+use crate::record::Record;
 use crate::versioning;
 use crate::writer;
 use std::io::{Read, Write};
@@ -46,188 +44,19 @@ impl<'a> Builder<'a> {
 
     /// Writes the binary contents of the SAILAR module to the specified destination.
     pub fn write_to<W: Write>(&self, destination: W) -> std::io::Result<()> {
-        use writer::{Result, VecWriter, Writer};
-
-        let mut wrapper = Writer::new(destination);
+        let mut wrapper = writer::Writer::new(destination);
         let out = &mut wrapper;
 
         out.write_all(crate::binary::MAGIC)?;
         out.write_all(&[self.format_version.major, self.format_version.minor])?;
         out.write_length(self.records.len())?;
 
-        fn write_record_content(out: &mut VecWriter, record: &Record) -> Result {
-            fn write_metadata_field(out: &mut VecWriter, field: &record::MetadataField) -> Result {
-                out.write_identifier(field.field_name())?;
-                match field {
-                    record::MetadataField::ModuleIdentifier(identifier) => {
-                        out.write_identifier(identifier.name())?;
-                        out.write_length(identifier.version().len())?;
-                        for number in identifier.version().iter() {
-                            out.write_unsigned_integer(*number)?;
-                        }
-                    }
-                    record::MetadataField::EntryPoint(entry) => out.write_length(*entry)?,
-                }
-
-                Ok(())
-            }
-
-            fn write_type_signature(out: &mut VecWriter, signature: &signature::Type) -> Result {
-                match signature {
-                    signature::Type::FixedInteger(ty) => {
-                        if *ty == signature::IntegerType::U8 {
-                            out.write_byte(signature::TypeCode::U8.into())
-                        } else if *ty == signature::IntegerType::S8 {
-                            out.write_byte(signature::TypeCode::S8.into())
-                        } else if *ty == signature::IntegerType::U16 {
-                            out.write_byte(signature::TypeCode::U16.into())
-                        } else if *ty == signature::IntegerType::S16 {
-                            out.write_byte(signature::TypeCode::S16.into())
-                        } else if *ty == signature::IntegerType::U32 {
-                            out.write_byte(signature::TypeCode::U32.into())
-                        } else if *ty == signature::IntegerType::S32 {
-                            out.write_byte(signature::TypeCode::S32.into())
-                        } else if *ty == signature::IntegerType::U64 {
-                            out.write_byte(signature::TypeCode::U64.into())
-                        } else if *ty == signature::IntegerType::S64 {
-                            out.write_byte(signature::TypeCode::S64.into())
-                        } else if *ty == signature::IntegerType::U128 {
-                            out.write_byte(signature::TypeCode::U128.into())
-                        } else if *ty == signature::IntegerType::S128 {
-                            out.write_byte(signature::TypeCode::S128.into())
-                        } else if *ty == signature::IntegerType::U256 {
-                            out.write_byte(signature::TypeCode::U256.into())
-                        } else if *ty == signature::IntegerType::S256 {
-                            out.write_byte(signature::TypeCode::S256.into())
-                        } else {
-                            let code = match ty.sign() {
-                                signature::IntegerSign::Unsigned => signature::TypeCode::UInt,
-                                signature::IntegerSign::Signed => signature::TypeCode::SInt,
-                            };
-
-                            out.write_byte(code.into())?;
-                            out.write_length(ty.size())
-                        }
-                    }
-                    signature::Type::UAddr => out.write_byte(signature::TypeCode::UAddr.into()),
-                    signature::Type::SAddr => out.write_byte(signature::TypeCode::SAddr.into()),
-                    signature::Type::F32 => out.write_byte(signature::TypeCode::F32.into()),
-                    signature::Type::F64 => out.write_byte(signature::TypeCode::F64.into()),
-                    signature::Type::RawPtr(Some(index)) => {
-                        out.write_byte(signature::TypeCode::RawPtr.into())?;
-                        out.write_length(*index)
-                    }
-                    signature::Type::FuncPtr(index) => {
-                        out.write_byte(signature::TypeCode::FuncPtr.into())?;
-                        out.write_length(*index)
-                    }
-                    signature::Type::RawPtr(None) => out.write_byte(signature::TypeCode::VoidPtr.into()),
-                }
-            }
-
-            fn write_function_signature(out: &mut VecWriter, signature: &signature::Function) -> Result {
-                out.write_length(signature.return_type_len())?;
-                out.write_length(signature.types().len() - signature.return_type_len())?;
-                for index in signature.types().iter() {
-                    out.write_length(*index)?;
-                }
-                Ok(())
-            }
-
-            fn write_code_value(out: &mut VecWriter, value: &instruction::Value) -> Result {
-                let flags = value.flags();
-                out.write_byte(flags.bits())?;
-                match value {
-                    instruction::Value::IndexedRegister(index) => out.write_length(*index),
-                    instruction::Value::Constant(instruction::Constant::Integer(integer)) => match integer {
-                        _ if flags.contains(instruction::ValueFlags::INTEGER_IS_EMBEDDED) => Ok(()),
-                        instruction::ConstantInteger::I8(byte) => out.write_byte(*byte),
-                        instruction::ConstantInteger::I16(ref bytes) => out.write_all(bytes),
-                        instruction::ConstantInteger::I32(ref bytes) => out.write_all(bytes),
-                        instruction::ConstantInteger::I64(ref bytes) => out.write_all(bytes),
-                    },
-                }
-            }
-
-            fn write_code_block(out: &mut VecWriter, block: &record::CodeBlock) -> Result {
-                out.write_length(block.input_count)?;
-                out.write_length(block.result_count)?;
-                out.write_length(block.temporary_count())?;
-                for index in block.register_types.iter() {
-                    out.write_length(*index)?;
-                }
-
-                out.write_length(block.instructions.len())?;
-                for instruction in block.instructions.iter() {
-                    out.write_byte(u8::from(instruction.opcode()))?;
-                    match instruction {
-                        Instruction::Nop | Instruction::Break => (),
-                        Instruction::Ret(values) => {
-                            out.write_length(values.len())?;
-                            for v in values.iter() {
-                                write_code_value(out, v)?;
-                            }
-                        }
-                        Instruction::Call(callee, arguments) => {
-                            out.write_length(*callee)?;
-                            out.write_length(arguments.len())?;
-                            for a in arguments.iter() {
-                                write_code_value(out, a)?;
-                            }
-                        }
-                        Instruction::AddI(operands) | Instruction::SubI(operands) => {
-                            out.write_byte(u8::from(operands.overflow_behavior()))?;
-                            write_code_value(out, operands.x_value())?;
-                            write_code_value(out, operands.y_value())?;
-                        }
-                    }
-                }
-
-                Ok(())
-            }
-
-            fn write_function_definition(out: &mut VecWriter, definition: &record::FunctionDefinition) -> Result {
-                out.write_unsigned_integer(definition.flags())?;
-                out.write_length(definition.signature)?;
-
-                match &definition.body {
-                    record::FunctionBody::Definition(index) => out.write_length(*index),
-                    record::FunctionBody::Foreign { library, entry_point } => {
-                        out.write_length(*library)?;
-                        out.write_identifier(entry_point)
-                    }
-                }
-            }
-
-            fn write_function_instantiation(out: &mut VecWriter, instantiation: &record::FunctionInstantiation) -> Result {
-                out.write_unsigned_integer(instantiation.export.kind().bits())?;
-
-                if let Some(symbol) = instantiation.export.symbol() {
-                    out.write_identifier(symbol)?;
-                }
-
-                out.write_length(instantiation.template)?;
-                out.write_length(0usize)
-            }
-
-            match record {
-                Record::MetadataField(field) => write_metadata_field(out, field),
-                Record::Identifier(identifier) => out.write_all(identifier.as_bytes()),
-                Record::TypeSignature(signature) => write_type_signature(out, signature),
-                Record::FunctionSignature(signature) => write_function_signature(out, signature),
-                Record::Data(bytes) => out.write_all(bytes.as_ref().as_bytes()),
-                Record::CodeBlock(block) => write_code_block(out, block),
-                Record::FunctionDefinition(definition) => write_function_definition(out, definition.as_ref()),
-                Record::FunctionInstantiation(instantiation) => write_function_instantiation(out, instantiation.as_ref()),
-            }
-        }
-
         let mut content_buffer = Vec::with_capacity(64);
 
         for record in self.records.iter() {
             content_buffer.clear();
-            let mut record_content = VecWriter::new(&mut content_buffer);
-            write_record_content(&mut record_content, record)?;
+            let mut record_content = writer::VecWriter::new(&mut content_buffer);
+            record_content.write_record_content(record)?;
             out.write_byte(u8::from(record.record_type()))?;
             out.write_length(content_buffer.len())?;
             out.write_all(&content_buffer)?;
--- sailar/src/writer.rs
+++ sailar/src/writer.rs
@@ -1,8 +1,11 @@
 //! Low-level internal API for writing the contents of a SAILAR binary module.
 
 use crate::identifier::Id;
+use crate::instruction::{self, Instruction};
 use crate::num::VarU28;
-use std::io::Write;
+use crate::record::{self, Record};
+use crate::signature;
+use std::io::{Error, ErrorKind, Write};
 
 pub type Result = std::io::Result<()>;
 
@@ -33,7 +36,7 @@ impl<W: Write> Writer<W> {
     {
         match value.try_into() {
             Ok(value) => self.write_unsigned_integer(value),
-            Err(err) => Err(std::io::Error::new(std::io::ErrorKind::InvalidInput, err)),
+            Err(err) => Err(Error::new(ErrorKind::InvalidInput, err)),
         }
     }
 
@@ -41,6 +44,161 @@ impl<W: Write> Writer<W> {
         let bytes = identifier.as_bytes();
         self.write_length(bytes.len())?;
         self.write_all(bytes)
+    }
+
+    fn write_export(&mut self, export: &record::Export) -> Result {
+        self.write_unsigned_integer(export.flag_bits().map_err(|e| Error::new(ErrorKind::InvalidInput, e))?)?;
+        if let Some(symbol) = export.symbol() {
+            self.write_all(symbol.as_bytes())?;
+        }
+        Ok(())
+    }
+
+    fn write_metadata_field(&mut self, field: &record::MetadataField) -> Result {
+        self.write_identifier(field.field_name())?;
+        match field {
+            record::MetadataField::ModuleIdentifier(identifier) => {
+                self.write_identifier(identifier.name())?;
+                self.write_length(identifier.version().len())?;
+                for number in identifier.version().iter() {
+                    self.write_unsigned_integer(*number)?;
+                }
+            }
+            record::MetadataField::EntryPoint(entry) => self.write_length(*entry)?,
+        }
+
+        Ok(())
+    }
+
+    fn write_type_signature(&mut self, signature: &signature::Type) -> Result {
+        match signature {
+            signature::Type::FixedInteger(ty) => {
+                if *ty == signature::IntegerType::U8 {
+                    self.write_byte(signature::TypeCode::U8.into())
+                } else if *ty == signature::IntegerType::S8 {
+                    self.write_byte(signature::TypeCode::S8.into())
+                } else if *ty == signature::IntegerType::U16 {
+                    self.write_byte(signature::TypeCode::U16.into())
+                } else if *ty == signature::IntegerType::S16 {
+                    self.write_byte(signature::TypeCode::S16.into())
+                } else if *ty == signature::IntegerType::U32 {
+                    self.write_byte(signature::TypeCode::U32.into())
+                } else if *ty == signature::IntegerType::S32 {
+                    self.write_byte(signature::TypeCode::S32.into())
+                } else if *ty == signature::IntegerType::U64 {
+                    self.write_byte(signature::TypeCode::U64.into())
+                } else if *ty == signature::IntegerType::S64 {
+                    self.write_byte(signature::TypeCode::S64.into())
+                } else if *ty == signature::IntegerType::U128 {
+                    self.write_byte(signature::TypeCode::U128.into())
+                } else if *ty == signature::IntegerType::S128 {
+                    self.write_byte(signature::TypeCode::S128.into())
+                } else if *ty == signature::IntegerType::U256 {
+                    self.write_byte(signature::TypeCode::U256.into())
+                } else if *ty == signature::IntegerType::S256 {
+                    self.write_byte(signature::TypeCode::S256.into())
+                } else {
+                    let code = match ty.sign() {
+                        signature::IntegerSign::Unsigned => signature::TypeCode::UInt,
+                        signature::IntegerSign::Signed => signature::TypeCode::SInt,
+                    };
+
+                    self.write_byte(code.into())?;
+                    self.write_length(ty.size())
+                }
+            }
+            signature::Type::UAddr => self.write_byte(signature::TypeCode::UAddr.into()),
+            signature::Type::SAddr => self.write_byte(signature::TypeCode::SAddr.into()),
+            signature::Type::F32 => self.write_byte(signature::TypeCode::F32.into()),
+            signature::Type::F64 => self.write_byte(signature::TypeCode::F64.into()),
+            signature::Type::RawPtr(Some(index)) => {
+                self.write_byte(signature::TypeCode::RawPtr.into())?;
+                self.write_length(*index)
+            }
+            signature::Type::FuncPtr(index) => {
+                self.write_byte(signature::TypeCode::FuncPtr.into())?;
+                self.write_length(*index)
+            }
+            signature::Type::RawPtr(None) => self.write_byte(signature::TypeCode::VoidPtr.into()),
+        }
+    }
+
+    fn write_function_signature(&mut self, signature: &signature::Function) -> Result {
+        self.write_length(signature.return_type_len())?;
+        self.write_length(signature.types().len() - signature.return_type_len())?;
+        signature.types().iter().try_for_each(|index| self.write_length(*index))
+    }
+
+    fn write_code_value(&mut self, value: &instruction::Value) -> Result {
+        let flags = value.flags();
+        self.write_byte(flags.bits())?;
+        match value {
+            instruction::Value::IndexedRegister(index) => self.write_length(*index),
+            instruction::Value::Constant(instruction::Constant::Integer(integer)) => match integer {
+                _ if flags.contains(instruction::ValueFlags::INTEGER_IS_EMBEDDED) => Ok(()),
+                instruction::ConstantInteger::I8(byte) => self.write_byte(*byte),
+                instruction::ConstantInteger::I16(ref bytes) => self.write_all(bytes),
+                instruction::ConstantInteger::I32(ref bytes) => self.write_all(bytes),
+                instruction::ConstantInteger::I64(ref bytes) => self.write_all(bytes),
+            },
+        }
+    }
+
+    fn write_code_block(&mut self, block: &record::CodeBlock) -> Result {
+        self.write_length(block.input_count)?;
+        self.write_length(block.result_count)?;
+        self.write_length(block.temporary_count())?;
+        for index in block.register_types.iter() {
+            self.write_length(*index)?;
+        }
+
+        self.write_length(block.instructions.len())?;
+        for instruction in block.instructions.iter() {
+            self.write_byte(u8::from(instruction.opcode()))?;
+            match instruction {
+                Instruction::Nop | Instruction::Break => (),
+                Instruction::Ret(values) => {
+                    self.write_length(values.len())?;
+                    values.iter().try_for_each(|value| self.write_code_value(value))?;
+                }
+                Instruction::Call(callee, arguments) => {
+                    self.write_length(*callee)?;
+                    self.write_length(arguments.len())?;
+                    arguments.iter().try_for_each(|argument| self.write_code_value(argument))?;
+                }
+                Instruction::AddI(operands) | Instruction::SubI(operands) => {
+                    self.write_byte(u8::from(operands.overflow_behavior()))?;
+                    self.write_code_value(operands.x_value())?;
+                    self.write_code_value(operands.y_value())?;
+                }
+            }
+        }
+
+        Ok(())
+    }
+
+    fn write_function_template(&mut self, template: &record::FunctionTemplate) -> Result {
+        self.write_export(&template.export)?;
+        self.write_length(template.signature)?;
+        self.write_length(template.entry_block)
+    }
+
+    fn write_function(&mut self, function: &record::Function) -> Result {
+        self.write_length(function.template)?;
+        self.write_unsigned_integer(0u8)
+    }
+
+    pub fn write_record_content(&mut self, record: &Record) -> Result {
+        match record {
+            Record::MetadataField(field) => self.write_metadata_field(field),
+            Record::Identifier(identifier) => self.write_all(identifier.as_bytes()),
+            Record::TypeSignature(signature) => self.write_type_signature(signature),
+            Record::FunctionSignature(signature) => self.write_function_signature(signature),
+            Record::Data(bytes) => self.write_all(bytes.as_ref().as_bytes()),
+            Record::CodeBlock(block) => self.write_code_block(block),
+            Record::FunctionTemplate(template) => self.write_function_template(template),
+            Record::Function(function) => self.write_function(function),
+        }
     }
 }
 
